f93365d722c29f583c511ac5b511c52f40178069
Jishnu Devarapalli <srijishnu.devarapalli@student.allenisd.org>
2022-10-08 12:24:40 -0500

    new

--- main.swift
+++ main.swift
@@ -1,23 +1,16 @@
-var integers:[String] = ["zebra" ,"cat", "dog", "monkey", "banana", "elephant", "apple", "boron"]
-
-
+var integers = [String]()
+while let line = readLine() {
+    integers.append(line)
+}
 var last_position = integers.count - 1
 var sap = true
 while sap == true {
     sap = false
     for i in 0..<last_position {
         if integers[i] > integers[i + 1] {
-
-
             integers.swapAt(i, i+1)
-
-
             sap = true
         }
-
-
     }
-  
-
 }
-print(integers)
+
